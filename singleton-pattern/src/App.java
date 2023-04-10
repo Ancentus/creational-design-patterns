ae4dcaf23d27a69fb6182bfb0f7968bd93c09a47
Ancentus <mancentus7@gmail.com>
2023-04-11 02:46:41 +0300

    Test singleton instance

--- singleton-pattern/src/App.java
+++ singleton-pattern/src/App.java
@@ -1,5 +1,21 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Get the singleton instance of PatientRecordManager
+        PatientRecordsManager recordsManager1 = PatientRecordsManager.getInstance();
+
+        // Call methods on the singleton instance
+        recordsManager1.addPatientRecord("John Doe", "Fever");
+        recordsManager1.viewPatientRecords("John Doe");
+
+        // Try to create another instance (which should be the same as recordsManager1)
+        PatientRecordsManager recordsManager2 = PatientRecordsManager.getInstance();
+
+        // Call methods on the new instance
+        recordsManager2.addPatientRecord("Jane Smith", "Headache");
+        recordsManager2.viewPatientRecords("Jane Smith");
+
+        // Check if recordsManager1 and recordsManager2 are the same instance
+        System.out.println("recordsManager1 == recordsManager2 : " +
+                (recordsManager1 == recordsManager2));
     }
 }
